90730f56f6db12072762ee830edd11d29d64d825
Tung Dao <daotung492@gmail.com>
2023-09-26 18:20:54 +0700

    villa number crud UI

---
+++ CleanArchitecture.WebUI/Controllers/VillaNumberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update(int villaNumberId)
+        {
+            return View();
+        }
+        public IActionResult Delete(int villaNumberId)
+        {
+            return View();
+        }
+    }
+}
