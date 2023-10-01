403bf651d7e3fcb6d54c0880dce51f68e792f63e
Tung Dao <daotung492@gmail.com>
2023-10-02 00:16:07 +0700

    prepare view home and detail page

---
+++ CleanArchitecture.WebUI/Models/ViewModel/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.WebUI.Models.ViewModel
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
